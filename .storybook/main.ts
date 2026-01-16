1d8d5fd59f23c603a977f07e28f924bfe830dfec
moz999 <tohosouth309+dev@gmail.com>
2026-01-16 16:07:03 +0900

    init

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import path from 'node:path'
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
+  ],
+  "framework": "@storybook/react-vite",
+  viteFinal: async (config) => {
+  config.resolve = config.resolve || {}
+  config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, '../src'),
+    }
+    return config
+  },
+};
+export default config;
